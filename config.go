82bb5f783a1f8d18811e919a80b602a032ec83ce
Claes Persson <c@cp.se>
2023-11-26 21:59:35 +0100

    Configuration structure
    Structure to hold configuration values, and load default values.

---
+++ config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Port int `json:"port"`
+}
+
+func loadConfigDefaults() Config {
+	config := Config{}
+
+	config.Port = 8081
+
+	return config
+}
+
+func loadConfig() Config {
+	cfg := loadConfigDefaults()
+
+	if os.Getenv("LABS_PORT") != "" {
+		port_i, port_int_err := strconv.Atoi(os.Getenv("LABS_PORT"))
+		if port_int_err == nil {
+			cfg.Port = port_i
+		}
+	}
+
+	return cfg
+}
--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
+var (
+	APP_NAME   = "labs"
+	APP_CONFIG Config
+	MAJOR      = 0
+	MINOR      = 0
+	REVISION   = 231126
+)
+
+func version() string {
+	return fmt.Sprintf("%s v.%d.%d.%d", APP_NAME, MAJOR, MINOR, REVISION)
+}
+
 func main() {
-	fmt.Println("labs")
+	log.Println(version())
+
+	APP_CONFIG = loadConfig()
+
+	app := fiber.New(fiber.Config{})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Listen(fmt.Sprintf(":%d", APP_CONFIG.Port))
 }
